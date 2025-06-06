6077267fdfcb68c51b0a29a8c0b060fadba2dd21
Lumbasi <154761897+LUMBASI14@users.noreply.github.com>
2025-06-06 12:00:48 +0300

    Create main.py

---
+++ modules_prac/main.py
@@ -0,0 +1,6 @@
+from modules_prac import calculator
+
+sum = calculator.addition(5,3)
+difference = calculator.subtraction(5,3)
+product = calculator.multiplication(5,3)
+quotient = calculator.division(5,3)
